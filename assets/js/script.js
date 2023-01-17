d63931ff9ba7559e2e9fd19414efefb098ec9f8c
AWatson1711 <a.watson1711@gmail.com>
2023-01-17 16:35:39 +0100

    First page finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,14 @@ import { getPokemon } from "./getPokemon.js"
 
 // Declaration d'element
 const pokemonsContainer = document.getElementById('pokemonsContainer');
+const modalContainer = document.querySelector('.modalContainer');
+const mainElt = document.querySelector('main');
+
+// Btn close modal
+const btnClose = document.createElement('button');
+btnClose.setAttribute('class', 'btn');
+
+
 
 async function showPokemons(pokemons){
     // Declaration des th
@@ -66,6 +74,7 @@ async function showPokemons(pokemons){
         for (const type of pokemon.apiTypes) {
             const pokemonType = document.createElement('img');
             pokemonType.setAttribute('src', type.image);
+            pokemonType.setAttribute('title', type.name);
             pokemonType.setAttribute('class', 'pokemonType')
             tdType.append(pokemonType);
         };
@@ -107,17 +116,41 @@ async function showPokemons(pokemons){
 
         
         
-        
-
-        
- 
-
-
-        
         trElement.append(tdImg, tdName, tdType, tdNbr, tdPv,tdAtq, tdDef, tdSpeDef, tdSpeAtq, tdVit);
         pokemonsContainer.appendChild(trElement);
+
+        linkElement.addEventListener('click', (e)=>{
+            // Modal display
+            const modal = document.createElement('div');
+            modal.setAttribute('class', 'modal');
+            // Modal img
+            const modalPokemonImg = document.createElement('img');
+            modalPokemonImg.setAttribute('class', 'modalImg');
+            modalPokemonImg.setAttribute('src', pokemon.image);
+
+            // Modal Description
+            const modalPokemonDes = document.createElement('p');
+            const textModal = document.createTextNode(`${pokemon.name} ipsum dolor sit amet, consectetur adipisicing elit. Delectus molestiae accusantium fugit? Molestiae, architecto ipsa tempora natus, cupiditate amet sit qui corrupti voluptatibus id voluptates, eaque vero delectus non facilis.ipsum dolor sit amet, consectetur adipisicing elit. Delectus molestiae accusantium fugit? Molestiae, architecto ipsa tempora natus, cupiditate amet sit qui corrupti voluptatibus id voluptates, eaque vero delectus non facilis.`);
+            modalPokemonDes.appendChild(textModal);
+            // Affectation du button close dans la modal
+            modal.append(modalPokemonImg, btnClose, modalPokemonDes);
+
+            
+            modalContainer.appendChild(modal);
+            modal.classList.add('modalDisplay');
+            mainElt.classList.add('overlay');
+
+            e.preventDefault();
+            btnClose.addEventListener('click', ()=>{
+                // Modal close
+                modal.classList.remove('modalDisplay');
+                mainElt.classList.remove('overlay');
+                
+            })
+        });
+      
     
-}
+};
 
 };
 
